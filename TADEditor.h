d6c8fd65ecf5553b1fd8c66d222bbd8f9b17ab82
Rafael Silva Damacena <103199315+ralphsenna@users.noreply.github.com>
2022-09-13 09:31:11 -0300

    Commit

--- TADEditor.h
+++ TADEditor.h
@@ -15,8 +15,48 @@ struct linha
 
 typedef struct linha Linha;
 
-
-void Init(Linha **Texto)
+struct descritor
 {
-    *Texto = NULL;
+    Linha *Inicio, *Fim;
+};
+
+typedef struct descritor Descritor;
+
+
+void Init(Descritor *D)
+{
+    (*D).Inicio = NULL;
+    (*D).Fim = NULL;
+}
+
+void Abrir(Descritor *D)
+{
+    FILE *Texto;
+    char Caminho[100], Linha[80];
+    int i;
+    printf("Digite o caminho para o arquivo: ");
+    gets(Caminho);
+    Texto = fopen(Caminho, "r");
+    fgets(Linha, 80, Texto);
+    while(feof(Texto))
+    {
+        for (i=0; i<strlen(Linha); i++)
+        {
+            if (Linha[i]=='\n')     
+                InsereLinhaVazia();
+            else
+                InsereCaracter();
+        }
+        fgets(Linha, 80, Texto);
+    }
+}
+
+void InsereCaracter()
+{
+
+}
+
+void InsereLinhaVazia()
+{
+    
 }
--- Trabalho - Editor de Texto.c
+++ Trabalho - Editor de Texto.c
@@ -6,22 +6,91 @@
 #include "TADEditor.h"
 
 
-void TelaPrincipal(void)
+void TelaPrincipal(Descritor *Desc)
 {
+    char Opcao;
+    system("cls");
     printf("F2-Abrir       F3-Salvar       F4-Sair       F5-Exibir\n");
-    printf("-------------------------------------------------------------------------------\n\n\n\n\n\n");
+    printf("-------------------------------------------------------------------------------\n\n");
     printf("-------------------------------------------------------------------------------\n");
-    printf("COL=,  LIN=   INSERT |||| LISTA");
+    printf("COL=,  LIN=   INSERT |||| LISTA"); //adicionar col e lin
+    do
+    {
+        Opcao = getche();
+        if (Opcao==0)
+        {
+            Opcao = getche();
+            switch(Opcao)
+            {
+                case 60:
+                        Abrir(&*Desc);
+                        break;
+
+                case 61:
+                        printf("F3\n");
+                        break;
+
+                case 63:
+                        printf("F5\n");
+                        break;
+
+                case 67:
+                        printf("F10\n");
+                        break;
+            }
+        }
+        else if (Opcao==224)
+        {
+            Opcao = getche();
+            switch(Opcao)
+            {
+                case 'A':
+                        break;
+
+                case 'B':
+                        break;
+
+                case 'C':
+                        break;
+
+                case 'D':
+                        break;
+
+                case 'E':
+                        break;
+            }
+        }
+        else
+        {
+            switch(Opcao)
+            {
+                case 'A':
+                        break;
+
+                case 'B':
+                        break;
+
+                case 'C':
+                        break;
+
+                case 'D':
+                        break;
+
+                case 'E':
+                        break;
+            }
+        }
+    }while(Opcao!=62 );
 }
 
 void MenuParametros(int *Linha, int *RecEsq, int *RecDir)
 {
     system("cls");
     printf("\t\t** EDITOR DE TEXTO **\n\n");
-	printf("### Menu de Configuracao de Parametros ###\n\n");
-	printf("Primeira Linha: ");
+    printf("### Menu de Configuracao de Parametros ###\n\n");
+    printf("Primeira Linha: ");
     scanf("%d", &*Linha);
-	printf("Recuo Esquerdo: ");
+    printf("Recuo Esquerdo: ");
     scanf("%d", &*RecEsq);
     printf("Recuo Direito: ");
     scanf("%d", &*RecDir);
@@ -29,10 +98,9 @@ void MenuParametros(int *Linha, int *RecEsq, int *RecDir)
 
 int main(void)
 {
-    Linha *Texto;
-    int Linha, RecEsq, RecDir;
-    Init(&Texto);
-    MenuParametros(&Linha, &RecEsq, &RecDir);
-    TelaPrincipal();
+    Descritor Descritor;
+    //int Linha, RecEsq, RecDir;
+    Init(&Descritor);
+    TelaPrincipal(&Descritor);
     return 0;
 }
